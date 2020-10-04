69f43bd545710ae1256cc792db30213610444c67
anurupsdev <72339452+anurupsdev@users.noreply.github.com>
2020-10-04 16:05:53 +0530

    Codeforces

---
+++ trail.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long int a,b,t,i;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+	return 0;
+}
