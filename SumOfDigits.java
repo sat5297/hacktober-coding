e4917b1f1b2c409a5e80dd65efd52d1deee129c6
Phoebe Lin <phoebe1318@yahoo.com>
2020-10-05 09:17:25 -0500

    Added program to find Sum of Digits

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -3,19 +3,20 @@ import java.io.*;
 
 class SumOfDigits{
    public static void main(String[] args) throws IOException{
-      // take in row of numbers separated by spaces
+      //Take in row of numbers separated by spaces
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      //change numbers to int and add into an arrayList
+      //Change numbers to int and add into an arrayList
       System.out.print("Enter numbers with spaces between them here ==> ");
       String nums[] = br.readLine().split(" ");
-      //for loop to loop through all numbers and add
+      //Loop through all numbers and add
       int sum = 0;
       for(int i = 0; i < nums.length; i++){
          sum+=Integer.parseInt(nums[i]);
       }
       System.out.println();
+      //Print out sum
       System.out.print("Sum: " + sum);
-      //print out sum
+      
    }
    
 }
