a3887c9844af2e50e11cc6d057ce11933feb76db
vikas-099 <42576154+vikas-099@users.noreply.github.com>
2020-10-06 14:41:23 +0530

    Create file for postorder traversal in binary tree

---
+++ post_order_traversal_binaryTree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data; // Data for the tree
+    struct Node *lt, *rt; //Nodes for the tree
+    Node(int data)
+    {
+        this->data = data;
+        lt = rt = NULL;
+    }
+};
+
+void printPostorder(struct Node* node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printPostorder(node->lt);
+    printPostorder(node->rt);
+    cout << node->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(20);
+    root->lt = new Node(41);
+    root->rt = new Node(52);
+    root->lt->lt = new Node(71);
+    root->lt->rt = new Node(89);
+    root->rt->lt = new Node(64);
+    root->rt->rt = new Node(37);
+    cout << "\nOutput for postorder traversal of binary tree is: ";
+    printPostorder(root);
+    return 0;
+}
