04dc26d49f714f26a5760c4d8d768628d29a79a4
Tia Low <lowtia@gmail.com>
2020-10-24 13:44:04 -0700

    Comparison added, solution code

--- identicalTrees.js
+++ identicalTrees.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // write a function to determine if two trees are identical 
+
 // functions signature will be to accept two trees as parameters and return a boolean true if they're identical, false if not
-// approach is to use in-order traversal of each tree to populate an array with the values, and then compare the two arrays
+
+// approach is to use in-order traversal of each tree to populate an array with the values, and then compare the two arrays to see if they're identical
 
 // assumes the tree inputs have basic tree properties on them like root, root.left and root.right
 
@@ -31,10 +33,12 @@ function determineIdentical(tree1, tree2) {
 
   _preOrder1(tree1.root);
 
-
   _preOrder2(tree2.root);
 
-
-
+  if(tree1Arr === tree2Arr){
+    return true;
+  } else {
+    return false;
+  }
 
 }
